708bcbdb160735f2c8a9fbf74b3948ab33d49cf1
marcosjunior <markosjr1969@gmail.com>
2026-02-17 18:26:24 -0300

    Java Basico - Curso Fiasco

---
+++ src/TIposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,7 @@
+package TIposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TIposDeDados/DadosPrimitivos.java
@@ -0,0 +1,29 @@
+package TIposDeDados;
+
+
+// PRINCIPAIS SHORTS CUT
+
+// SOUT -> System.out.println
+// PSVM -> public static void main
+// Shift F10 -> compilar arquivo
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados Primitivos: int, double, float, char, boolean, short.
+        * Objetivo: criar um ninja - Naruto
+        **/
+
+        int idade = 16; // Valor Maximo 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 99999L; // Valor maximo: 9 trilhoes e uns quebrados
+
+
+        System.out.println(idade);
+        System.out.println("saldo Bancario = " + saldoBancario);
+
+
+    }
+}
