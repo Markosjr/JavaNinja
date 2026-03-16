ddd19a498c17377febc2de06e4572890e3da15c4
marcosjunior <markosjr1969@gmail.com>
2026-03-16 20:09:53 -0300

    Desafio 01

---
+++ src/TIposDeDados/Desafio01.java
@@ -0,0 +1,34 @@
+package TIposDeDados;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        String nomeNinja1 = "Naruto";
+        int idadeNinja1 = 21;
+        String nomeMissao = "Massacre";
+
+        System.out.println("--------------Ninja 1 ------------");
+        System.out.println("Nome: " +nomeNinja1);
+        System.out.println("Idade: " +idadeNinja1);
+        System.out.println("Missao: "+ nomeMissao);
+
+        System.out.println("--------------Ninja 2 ------------");
+        String nomeNinja2 = "Sasuke";
+        int idadeNinja2 = 19;
+        String nomeMissao2 = "Espionagem";
+
+        System.out.println("Nome: " +nomeNinja2);
+        System.out.println("Idade: " +idadeNinja2);
+        System.out.println("Missao: "+ nomeMissao2);
+
+        System.out.println("--------------Ninja 3 ------------");
+        String nomeNinja3 = "Sakura";
+        int idadeNinja3 = 22;
+        String nomeMissao3 = "Captura";
+
+        System.out.println("Nome: " +nomeNinja3);
+        System.out.println("Idade: " +idadeNinja3);
+        System.out.println("Missao: "+ nomeMissao3);
+
+    }
+
+}
